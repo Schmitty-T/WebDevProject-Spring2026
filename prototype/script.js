73d81befd706bc6c471b03bbe0aff81bc1d96eab
David Mbagwu <davidmbagwu@gmail.com>
2026-04-12 08:47:55 -0500

    Uploaded homepage html code, css code, script file and its implementation of light and dark theme feature.

---
+++ prototype/script.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
